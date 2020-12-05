48d6f868c84eea395c33ec3ab4553f37b698d485
Taha Zeybek <tahazeybek5@gmail.com>
2020-12-05 16:39:13 +0300

    changes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
+import {StaticRouter} from "react-router"
 import "react-awesome-slider/dist/custom-animations/cube-animation.css";
 
 ReactDOM.render(
